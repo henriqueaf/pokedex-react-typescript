d4f27e9162391e7879a9bc01c1c3f4cd8ba6516d
Henrique Almeida <henrique_af1@msn.com>
2020-05-21 17:01:15 -0300

    Add Bootstrap to Project

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { PokemonProvider } from './contexts/PokemonContext';
 import PokemonList from './components/PokemonList';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 const App = () => (
