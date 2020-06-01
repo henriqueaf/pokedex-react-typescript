cdd3b4dde1bc2a450f5f846ef76b493e96ed8888
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-01 00:06:05 +0000

    Deploy henriqueaf/pokedex-react-typescript to github.com/henriqueaf/pokedex-react-typescript.git:gh-pages

---
+++ precache-manifest.3e5d6f4e929e67b9eaacd2309e3d5fd9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef470171e21f1753c59fb91bc0db92ee",
+    "url": "/pokedex-react-typescript/index.html"
+  },
+  {
+    "revision": "fabba08e462deefe26cf",
+    "url": "/pokedex-react-typescript/static/css/2.ae98c48f.chunk.css"
+  },
+  {
+    "revision": "936c39c17e7a58adfcda",
+    "url": "/pokedex-react-typescript/static/css/main.e11b59bb.chunk.css"
+  },
+  {
+    "revision": "fabba08e462deefe26cf",
+    "url": "/pokedex-react-typescript/static/js/2.a1d99f59.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/pokedex-react-typescript/static/js/2.a1d99f59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "936c39c17e7a58adfcda",
+    "url": "/pokedex-react-typescript/static/js/main.2ba04298.chunk.js"
+  },
+  {
+    "revision": "8558a94f5d04bbfe95d3",
+    "url": "/pokedex-react-typescript/static/js/runtime-main.f31f8716.js"
+  },
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/pokedex-react-typescript/static/media/pokeball.f10bf078.png"
+  }
+]);
